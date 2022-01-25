68fbfca94edc4c6a5ae1b94274a1beab3e0eb661
Stefan Mai <stefan.mai@iamnafets.com>
2022-01-24 23:18:00 -0800

    Trying to work through compiler bugs

--- src/IndexBinaryFlat.cpp
+++ src/IndexBinaryFlat.cpp
@@ -1,5 +1,5 @@
 #include "faiss-rust/include/IndexBinaryFlat.h"
-
+#include <memory>
 
 std::unique_ptr<faiss::IndexBinaryFlat> faiss::new_index_binary_flat(int64_t dims) {
   return std::unique_ptr<faiss::IndexBinaryFlat>(new faiss::IndexBinaryFlat(dims));
